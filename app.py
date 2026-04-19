1725317c0e5f556317b9a52d74c71fdd754bcf75
golden mane <dulackmugi@gmail.com>
2026-04-19 03:39:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,5 @@
-import os
 from flask import (
-    Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hello from Railway!"
-
-if __name__ == "__main__":
-    # Get port from environment or default to 5000
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port),
+    Flask,
     render_template,
     request,
     jsonify,
